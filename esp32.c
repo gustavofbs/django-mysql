6ec064be2f29c9c61e4ffbf3e4a358cffb9f7ccb
Gustavo <guguboasorte@gmail.com>
2024-05-31 16:52:08 -0300

    altera o banco

--- esp32.c
+++ esp32.c
@@ -2,78 +2,77 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
+// Definições dos pinos dos motores
+#define MOTOR_ESQUERDO_PIN_A 2  // Pino A do motor esquerdo
+#define MOTOR_ESQUERDO_PIN_B 3  // Pino B do motor esquerdo
+#define MOTOR_DIREITO_PIN_A 4   // Pino A do motor direito
+#define MOTOR_DIREITO_PIN_B 5   // Pino B do motor direito
+
 // WiFi credentials
-const char* ssid = "nome da rede wifi";
-const char* password = "senha da rede wifi";
+const char* ssid = "NOME DA REDE WIFI";
+const char* password = "SENHA DA REDE WIFI";
 
-// Endereço da API - certifique-se de usar o IP do servidor
-const char* apiUrl = "link do ngrok baseado na porta do servidor django/api/esp32/"; // Alterar para o IP correto do servidor
+// Endereço da API
+const char* apiUrl = "link gerado pelo ngrok/api/esp32/";
 
+// Função de inicialização
 void setup() {
-  // Inicializa a comunicação serial com taxa de 115200 bauds
+  pinMode(MOTOR_ESQUERDO_PIN_A, OUTPUT);
+  pinMode(MOTOR_ESQUERDO_PIN_B, OUTPUT);
+  pinMode(MOTOR_DIREITO_PIN_A, OUTPUT);
+  pinMode(MOTOR_DIREITO_PIN_B, OUTPUT);
+  
   Serial.begin(9600);
-  delay(1000);
-  
-  Serial.println("Iniciando a conexão ao WiFi...");
+
+  // Inicializa a conexão Wi-Fi
+  Serial.print("Conectando ao WiFi ");
   WiFi.begin(ssid, password);
-  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  
-  Serial.println("");
-  Serial.println("Conectado ao WiFi!");
-  Serial.print("Endereço IP: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(" conectado");
 }
 
+// Função principal
 void loop() {
-  // Simula dados fictícios dos sensores
-  int sensorEsquerdo = random(0, 2);  // Valor aleatório entre 0 e 1
-  int sensorDireito = random(0, 2);   // Valor aleatório entre 0 e 1
+  // Gerar dados simulados
+  int sensorEsquerdo = random(2);
+  int sensorDireito = random(2);
+  float velocidade_inst = random(50, 150); // Velocidade entre 50 e 150 unidades por segundo
+  float distancia_parcial = random(1000); // Distância entre 0 e 1000 unidades
 
-  // Imprime os dados dos sensores no Serial Monitor
-  Serial.print("Sensor Esquerdo: ");
-  Serial.println(sensorEsquerdo);
-  Serial.print("Sensor Direito: ");
-  Serial.println(sensorDireito);
-
-  // Envia os dados fictícios para a API
-  enviarDadosAPI(sensorEsquerdo, sensorDireito);
-  delay(10000); // Espera 10 segundos antes de enviar os dados novamente
+  // Envia os dados simulados para a API
+  enviarDadosAPI(sensorEsquerdo, sensorDireito, velocidade_inst, distancia_parcial);
+  delay(1000); // Espera um segundo antes de enviar os dados novamente
 }
 
-// Função para enviar dados para a API
-void enviarDadosAPI(int sensorEsquerdo, int sensorDireito) {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    
-    http.begin(apiUrl);
-    http.addHeader("Content-Type", "application/json");
-    
-    // Ajuste os nomes dos campos conforme a API espera
-    String jsonData = "{\"sensor_esquerdo\": " + String(sensorEsquerdo) + ", \"sensor_direito\": " + String(sensorDireito) + "}";
-    Serial.print("Enviando dados JSON: ");
-    Serial.println(jsonData);
-    
-    int httpResponseCode = http.POST(jsonData);
-    
-    if (httpResponseCode > 0) {
-      Serial.print("Requisição POST bem-sucedida. Código de resposta: ");
-      Serial.println(httpResponseCode);
-      String payload = http.getString();
-      Serial.println("Resposta do servidor: ");
-      Serial.println(payload);
-    } else {
-      Serial.print("Erro na requisição POST. Código de resposta: ");
-      Serial.println(httpResponseCode);
-      Serial.print("Mensagem de erro: ");
-      Serial.println(http.errorToString(httpResponseCode).c_str());
-    }
-    
-    http.end();
+// Função para enviar dados simulados para a API
+void enviarDadosAPI(int sensorEsquerdo, int sensorDireito, float velocidade_inst, float distancia_parcial) {
+  // Cria um objeto HTTPClient
+  HTTPClient http;
+  
+  // Adiciona o cabeçalho "Content-Type"
+  http.begin(apiUrl);
+  http.addHeader("Content-Type", "application/json");
+  
+  // Cria o JSON com os dados simulados
+  String jsonData = String("{\"sensor_esquerdo\": ") + sensorEsquerdo + String(", \"sensor_direito\": ") + sensorDireito + String(", \"velocidade\": ") + velocidade_inst + String(", \"distancia\": ") + distancia_parcial + String("}");
+
+  
+  // Envia a requisição POST com os dados JSON
+  int httpResponseCode = http.POST(jsonData);
+  
+  // Verifica o código de resposta
+  if (httpResponseCode > 0) {
+    Serial.print("Requisicao POST bem-sucedida. Codigo de resposta: ");
+    Serial.println(httpResponseCode);
   } else {
-    Serial.println("Erro na conexão WiFi");
+    Serial.print("Erro na requisicao POST. Codigo de resposta: ");
+    Serial.println(httpResponseCode);
   }
+  
+  // Libera os recursos
+  http.end();
 }
+
--- rastrobotDjango/serializers.py
+++ rastrobotDjango/serializers.py
@@ -4,4 +4,5 @@ from .models import ESP32Data
 class ESP32DataSerializer(serializers.ModelSerializer):
     class Meta:
         model = ESP32Data
-        fields = ['sensor_esquerdo', 'sensor_direito']
+        fields = ['sensor_esquerdo', 'sensor_direito', 'velocidade', 'distancia', 'timestamp']
+        read_only_fields = ['timestamp']
